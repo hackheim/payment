3ad50c1cc7e61f6bc066e5b619f0c82624b84d81
Torbjørn Kvåle <torbjoernkvaale@gmail.com>
2017-03-07 17:03:13 +0100

    hide company fields and add button to show them

--- views/details.php
+++ views/details.php
@@ -8,12 +8,20 @@
   <label for="coupon">Coupon code</label><br />
   <input type="text" name="coupon" value=""><br />
   <p class="note">Enter STUDENT if you are a full time student or PEON if you have a full time desk at work-work. (*)</p>
+  <p id="company_form_link">
+    <a href="#" style="font-size: 0.7em" 
+      onclick="document.getElementById('company_form_section').style.display='block';document.getElementById('company_form_link').style.display='none';">
+      Are you a <strong>company</strong>? Click here to add extra information
+    </a>
+  </p>
+  <div id="company_form_section" style="display:none">
   <label for="company_name">Company name:</label><br />
   <input type="text" name="company_name">
   <p class="note">Leave empty if you are a private person</p>
   <label for="organization_number">Organization Number:</label><br />
   <input type="text" name="organization_number">
   <p class="note">Leave empty if you are a private person</p>
+  </div>
   <input type="submit" value="Submit" /><br />
   <p class="note">* You will need to show documentation to prove student or work-work-peon status</p>
 </form>
